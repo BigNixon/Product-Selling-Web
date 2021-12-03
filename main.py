e3b6c46e903a133d6c500a17d03336b6b9b8c7fa
BigNixon <nixon.ortiz.d@uni.pe>
2021-12-03 01:37:44 -0500

    reordering folders to match with flask and admit the load of css file into html

---
+++ main.py
@@ -0,0 +1,31 @@
+import pymysql
+from flask.helpers import url_for
+from flask import Flask, render_template,request,redirect,flash
+from venta import *
+
+# Conection with database
+connection=pymysql.connect(
+            host="127.0.0.1", # si es remota coloca dominio // local tu IP
+            user='root',
+            password='Ortiz.007',
+            db='productosdb',)
+
+cursor = connection.cursor()
+
+
+app = Flask(__name__)
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/carrito.html')
+def carrito():
+    return render_template('carrito.html')
+
+@app.route('/admin.html')
+def administracion():
+    return render_template('admin.html')
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
